18d16674ce49ca2cbcf4f6319176a62e00fcacb9
Charles <barsubiacharles@gmail.com>
2025-02-25 12:36:39 +0800

    Adjusted layout for home hero page cta buttons and changed mobile nav portfolio to career button.

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -58,8 +58,9 @@ const toggleNavDropdown = () => {
                                 Services</NuxtLink>
                         </li>
                         <li>
-                            <a href="#"
-                                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Portfolio</a>
+                            <NuxtLink to="/careers" class="block py-2 pr-4 pl-3 rounded lg:p-0"
+                                :class="{ 'active-nav': route.name === 'careers', 'inactive-nav': route.name !== 'careers' }">
+                                Careers</NuxtLink>
                         </li>
                         <li>
                             <NuxtLink to="/about" class="block py-2 pr-4 pl-3 rounded lg:p-0"
