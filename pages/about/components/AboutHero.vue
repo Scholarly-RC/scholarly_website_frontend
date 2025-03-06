b7cf6c7bcf1d04cc2ef15dce4ec27b09e09ed3d4
Charles <barsubiacharles@gmail.com>
2025-03-07 04:46:17 +0800

    Removed redundant data checks.

--- pages/about/components/AboutHero.vue
+++ pages/about/components/AboutHero.vue
@@ -21,15 +21,15 @@ const { data, status, error } = useFetch(`${config.public.apiBaseUrl}/items/abou
                 class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-lg lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                 <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{{
-                        data?.data?.title }}</h2>
-                    <p class="mb-4">{{ data?.data?.description }}</p>
+                        data.data.title }}</h2>
+                    <p class="mb-4">{{ data.data.description }}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 mt-8">
                     <img class="w-full rounded-lg"
-                        :src="`${config.public.apiBaseUrl}/assets/${data?.data?.image_1}/?quality=85&format=webp`"
+                        :src="`${config.public.apiBaseUrl}/assets/${data.data.image_1}/?quality=85&format=webp`"
                         loading="lazy" alt="About Page Image 1">
                     <img class="mt-4 w-full lg:mt-10 rounded-lg"
-                        :src="`${config.public.apiBaseUrl}/assets/${data?.data?.image_2}/?quality=85&format=webp`"
+                        :src="`${config.public.apiBaseUrl}/assets/${data.data.image_2}/?quality=85&format=webp`"
                         loading="lazy" alt="About Page Image 2">
                 </div>
             </div>
